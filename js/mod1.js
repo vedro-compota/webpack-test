3a8a2ddea04ab8d6d19d9ac70f9599245c6021b7
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-01-16 15:06:50 +0300

    обн

--- js/mod1.js
+++ js/mod1.js
@@ -1,10 +1,13 @@
 export {hello1};
 import {helloCommon} from "./common";
+import {jswl} from "../node_modules/js-wrapper-lib/src/jswl.js";
 
 var hello1 = function()
 {
     console.log('hello 1');
     helloCommon();
 }
+
+jswl.hello();
 
 
